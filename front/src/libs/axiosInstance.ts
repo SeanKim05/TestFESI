fe8752900bfde6ac2cef174b9cf57e5e28caeff5
SeanKim05 <dbgus20609@gmail.com>
2024-10-16 22:37:14 +0900

    Test6

--- front/src/libs/axiosInstance.ts
+++ front/src/libs/axiosInstance.ts
@@ -5,10 +5,4 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-axiosInstance.interceptors.request.use(async (config) => {
-  if (typeof window !== "undefined") {
-  }
-  return config;
-});
-
 export default axiosInstance;
